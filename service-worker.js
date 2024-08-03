8f02facd0918c960943398ebfe3eb4534ae5a2e7
Mehboob Elahi <siljuapp@gmail.com>
2024-08-03 18:58:15 +0530

    aaa

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "elahi_study_app";
-const urlsToCache = ["/", "/index.html", "/styles.css", "/script.js", "/assets/esalogo_192.png", "/assets/esalogo_512.png"];
+const urlsToCache = ["/", "/index.html", "/styles.css", "/script.js", "/assets/esalogo192.png", "/assets/esalogo512.png"];
 
 self.addEventListener("install", function (event) {
     event.waitUntil(
