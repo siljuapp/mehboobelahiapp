9dc9b139e638f77bcf871e0d0d27fcd8dfda63de
Mehboob Elahi <siljuapp@gmail.com>
2024-11-13 19:07:55 +0530

    asdad

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
     content: ["*"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                loading: {
+                    "0%": { width: "0%" },
+                    "100%": { width: "100%" },
+                },
+            },
+            animation: {
+                loading: "loading 3s ease-in-out forwards",
+            },
+        },
     },
     plugins: [],
 };
