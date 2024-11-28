27786fd6a748a7468c9e37b16b90a320758d760f
Mehboob Elahi <siljuapp@gmail.com>
2024-11-29 00:31:00 +0530

    asdas

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install", (event) => {
-    console.log("Service Worker installing.");
-    // Add caching or pre-caching logic here
-});
-
-self.addEventListener("fetch", (event) => {
-    console.log("Service Worker fetching.", event.request.url);
-    // Add network or caching logic here
-});
